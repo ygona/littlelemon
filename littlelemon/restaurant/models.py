99434aec2ef73555343324d5fb6a574661d9f5ce
ygona <137731443+ygona@users.noreply.github.com>
2024-03-30 15:47:38 -0400

    restaurant-tables

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+   # ID = models.IntegerField(max = 11)
+    Name = models.CharField(max_length = 255)
+    No_of_guests = models.IntegerField()
+    Bookingdate = models.DateTimeField()
+
+
+class Menu(models.Model):
+   # ID = models.IntegerField(max = 5)
+    Title = models.CharField(max_length = 255)
+    Price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    Inventory = models.PositiveIntegerField()
